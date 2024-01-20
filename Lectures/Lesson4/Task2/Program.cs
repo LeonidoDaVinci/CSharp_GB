79616128724c6873aa146ab651db7c449890b2aa
Aleksei <lenik007@mail.ru>
2024-01-20 21:41:26 +0500

    lesson4 task2

---
+++ Lectures/Lesson4/Task2/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 2. Создать двумерный массив, состоящий из целых чисел, вывсти на экран
+// все элементы массива, если сумма их цифр четная. 
+
+int[,] CreateMatrix(int rowCount, int columsCount)
+{
+    int[,] matrix = new int[rowCount, columsCount];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 1000);
+        }
+    }
+    return matrix;
+}
+int[,] matrix = CreateMatrix(3, 4);
+
+void ShowMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+ShowMatrix(matrix);
+
+foreach (int e in matrix)
+{
+    if (isInteresting(e) == true)
+    {
+        Console.WriteLine(e);
+    }
+}
+
+bool isInteresting(int value)
+{
+    int sumOfDigits = GetSumOfDidgts(value);
+    if (sumOfDigits % 2 == 0)
+    {
+        return true;
+    }
+         return false;
+}
+
+int GetSumOfDidgts (int value)
+{
+    int sum = 0; 
+    while (value > 0)
+    {
+        sum = sum + value % 10;
+        value = value / 10; 
+    }
+    return sum;
+}
+
+
+
