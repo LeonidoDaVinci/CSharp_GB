9465fb489c78511ce733eebd23f1a7d20c966afa
Aleksei <lenik007@mail.ru>
2024-01-22 16:44:08 +0500

    Seminar4 (functions) Homework_Task1 (Бесконечный ввод с консоли пока не введут q или сумма цифр не будет четной

---
+++ Seminars/Sem4/HomeWork_Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+//Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+while (true)
+{
+    Console.Write("Введите число или q для выхода: ");
+    string input = Console.ReadLine()!;
+    if (input == "q")
+    {
+        break;
+    }
+    int number = Convert.ToInt32(input);
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    if (sum % 2 == 0)
+    {
+        break;
+    }
+}
