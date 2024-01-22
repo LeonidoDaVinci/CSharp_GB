53e84dd76e9baf69557c2f6b0f373508500541f2
Aleksei <lenik007@mail.ru>
2024-01-22 13:14:40 +0500

    Seminar4 (functions) HomeWork_Task2 (quantity of even elements)

--- Seminars/Sem4/HomeWork_Task2/Program.cs
+++ Seminars/Sem4/HomeWork_Task2/Program.cs
@@ -5,14 +5,14 @@ Console.Write("Задайте размер массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[size];
 Random rnd = new Random();
-int count = 0;  //счетчик четных элементов
+int count = 0;    //счетчик четных элементов
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = rnd.Next(100, 1000);  //создаем массив из трехзначных чисел
     Console.Write($"{array[i]} ");  //вывод каждого элемента массива 
-    if (array[i] % 2 == 0)   // проверяем элемент на четность 
+    if (array[i] % 2 == 0)   // проверяем элемент на четность (остаток от деления на 2)
     {
-        count++;
+        count++;  //если элемент массива четный, то счетчик увеличивается
     }
 }
 Console.WriteLine($"Количество четных элементов масива:  {count}");
