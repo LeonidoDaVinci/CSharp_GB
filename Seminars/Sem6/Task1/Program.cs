a3ff76d03169534b44297a8c06ef8815a81fbe2d
Aleksei <lenik007@mail.ru>
2024-01-28 14:23:05 +0500

    Seminar 6 (arrays and strings) Task1 Задать массив из символов char []  и перевести его в строку string

---
+++ Seminars/Sem6/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// СоздатьЗадать массив из символов char []  и перевести его в строку string
+
+string GetStringFromCharArray(char[] array)
+{
+    string str = ""; //StringBuilber 
+    foreach (char c in array)
+    {
+        str += c;
+    }
+    return str;
+}
+char[] chars = { '1', 'd', '!', '2', 'f' };
+string str = GetStringFromCharArray(chars);
+Console.Writeline(str);
