645ad704ae8d24c16ecd43f55c5cc6945ec34120
Aleksei <lenik007@mail.ru>
2024-01-28 14:25:38 +0500

    Seminar 6 (arrays and strings) Task3 Задать строку с консоли в нижнем регистре и вывести количество гласных букв

---
+++ Seminars/Sem6/task3/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задать строку с консоли в нижнем регистре и вывести количество гласных букв 
+int GetVovelsCount(string str)
+{
+    string vovels = "aoyuei"; //строка из гласных букв
+    int counter = 0;
+
+    foreach (char c in str)
+    {
+        // if (vovels.Contains(c))
+        // {
+        //     counter++;
+        // }
+        foreach (char vovel in vovels)
+        {
+            if (c == vovel)
+            {
+                counter++;
+            }
+        }
+    }
+    return counter;
+}
+string str = Console.ReadLine()!;
+int countVovels = GetVovelsCount(str);
+Console.WriteLine(countVovels);
