faa31aa07edb89e43022f9cdd81bf06f9bd6d629
Aleksei <lenik007@mail.ru>
2024-01-28 14:17:25 +0500

    Seminar6 (arrays and strings) Homework_Task2 Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные

---
+++ Seminars/Sem6/HomeWork_task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные
+// “aBcD1ef!-” => “abcd1ef!-” 
+
+string text = "abCDe4g6HB";
+
+string GetUpToLowChar(string str)
+{
+    string result = "";
+    foreach (char e in str.ToLower()) //перебиираем каждый символ в строке и делаем все строчными
+    {
+        result = result + e;  //записываем новую стоку из строчных символов
+    }
+    return result;
+}
+
+Console.WriteLine(GetUpToLowChar(text));
