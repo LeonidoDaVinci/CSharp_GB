b1c517cb87da9d1996d588116fb6216709ee95c6
Aleksei <lenik007@mail.ru>
2024-01-28 13:57:43 +0500

    Seminar 6 (arrays and strings) HomeWork Task1

---
+++ Seminars/Sem5/HomeWork_Autotest3/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 3. Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+int [,] numbers = new int[,]
+{
+    {1, 30, 40},
+    {1, 10, 0},
+    {7, 8, 2},
+    {0, 0, 1}
+};
+/// Вычисление сумм по строкам (на выходе массив с суммами строк)
+int[] SumRows(int[,] array)
+{
+    //Напишите свое решение здесь
+    int n = array.GetLength(0);
+    int[] ArrRow = new int[n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int SumElem = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            SumElem = SumElem + array[i, j];
+        }
+        ArrRow[i] = SumElem;
+    }
+    return ArrRow;
+}
+
+// Получение индекса минимального элемента в одномерном массиве
+int MinIndex(int[] array)
+{
+    //Напишите свое решение здесь
+    int MinElem = array[0];
+    int MinIndex = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (MinElem > array[i])
+        {
+            MinElem = array[i];
+            MinIndex = i;
+        }
+    }
+    return MinIndex;
+}
+void PrintResult(int[,] numbers)
+{
+    //Напишите свое решение здесь
+    int Row = MinIndex(SumRows(numbers));
+    Console.WriteLine(Row);
+}
+
+PrintResult(numbers);
---
+++ Seminars/Sem6/HomeWork_task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char[,]). Создать строку из символов этого массива. 
+char[,] charArray = {
+    {'a','b','c','w'},
+    {'d','e','f','q'}
+    };
+// Функция создания строки из двумерного массива (проход по строкам)
+string GetStringFromArray(char[,] ChArr)
+{
+    string s1 = "";
+    for (int i = 0; i < ChArr.GetLength(0); i++)
+    {
+        for (int j = 0; j < ChArr.GetLength(1); j++)
+        {
+            s1 = s1 + ChArr[i, j];
+        }
+    }
+    return s1;
+}
+
+string result = GetStringFromArray(charArray);
+Console.WriteLine(result);
