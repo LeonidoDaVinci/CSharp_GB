647963a7dc9707d2097e9a83436edd1be22e36dd
Aleksei <lenik007@mail.ru>
2024-01-28 14:21:28 +0500

    Seminar 6 (arrays and strings) HomeWork_Task3 Задайте произвольную строку. Выясните, является ли она палиндромом.

---
+++ Seminars/Sem6/HomeWork_task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+// “aBcD1ef!-” => Нет
+// “шалаш” => Да
+// “55655” => Да
+
+string text = Console.ReadLine()!;
+string revers = "";
+for (int i = text.Length - 1; i >= 0; i--)
+{
+    revers += text[i]; //записываем строку в реверсе начиная с последнего элемента
+}
+
+if (revers == text)
+{
+    Console.WriteLine($"Текст {text} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Текст {text} НЕ палиндром");
+}
