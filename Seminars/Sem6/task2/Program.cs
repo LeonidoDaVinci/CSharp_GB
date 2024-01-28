fccb12bd6d7c3be66e85e489db6c959bcc513a2e
Aleksei <lenik007@mail.ru>
2024-01-28 14:24:25 +0500

    Seminar 6 (arrays and strings) Task2 Cоздать строку и перевести ее в массив символов char[]

---
+++ Seminars/Sem6/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Cоздать строку и перевести ее в массив символов char[]
+char[] StringToArray(string str)
+{
+    char[] chars = new char[str.Length];
+    for (int i = 0; i < str.Length; i++)
+    {
+        chars[i] = str[i];
+    }
+    return chars;
+}
+
+string str = "Hello world";
+char[] chars = StringToArray(str);
+// foreach (char c in chars)
+// {
+Console.Write(chars);
+// }
