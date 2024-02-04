960d36ad32e0cbf286a4043b9d6fa03d738f62fd
Aleksei <lenik007@mail.ru>
2024-02-04 12:40:25 +0500

    Seminar7 (recursion) Task1 Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.

---
+++ Seminars/Sem7/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+void PrintNumber(int m, int n)
+{
+  if (n < m) return;
+  PrintNumber(m, n - 1);
+  Console.Write(n + ", ");
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+int m = InputNumbers("Введите M: ");
+int n = InputNumbers("Введите N: ");
+PrintNumber(m, n);
