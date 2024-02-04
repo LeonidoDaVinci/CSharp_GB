3ca25c62ae83d202b2e6de4977e737b5dadff5b4
Aleksei <lenik007@mail.ru>
2024-02-04 12:43:45 +0500

    Seminar7 (recursion) Task3 Задайте произвольный массив. Выведете его элементы, начиная с конца.

---
+++ Seminars/Sem7/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+int [] arr = {1,2,3,4,5};
+
+void PrintArray(int[] arr, int n)
+    {
+        if (n <= 0)
+            return;
+        else
+        {
+            Console.Write(arr[n - 1] + " ");
+            PrintArray(arr, n - 1);
+        }
+    }
+int len = arr.Length;
+PrintArray(arr, len);
